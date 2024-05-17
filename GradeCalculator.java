8d6e7a0abf91f53573455e72c6e70e34b24608aa
vishalgunasekaren <vishalgunasekaran010@gmail.com>
2024-05-17 23:29:04 +0530

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter marks obtained in each subject (out of 100):");
+
+        System.out.print("English: ");
+        int english = scanner.nextInt();
+
+        System.out.print("Tamil: ");
+        int tamil = scanner.nextInt();
+
+        System.out.print("Maths: ");
+        int maths = scanner.nextInt();
+
+        System.out.print("Science: ");
+        int science = scanner.nextInt();
+
+        System.out.print("Social Science: ");
+        int socialScience = scanner.nextInt();
+
+        // Calculate total marks
+        int totalMarks = english + tamil + maths + science + socialScience;
+
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / 5;
+
+        // Assign grades based on the average percentage
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Display results
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
